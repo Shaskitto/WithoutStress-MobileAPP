8948911bdc3404307dc398366119daad353bcaf0
Santierazo <santiagoerazoaragon@gmail.com>
2024-09-15 16:58:45 -0500

    Daily Plan

--- src/app/pages/plan/plan.page.ts
+++ src/app/pages/plan/plan.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.page.html',
   styleUrls: ['./plan.page.scss'],
 })
-export class PlanPage implements OnInit {
+export class PlanPage implements OnInit, ViewWillEnter {
+  user$: Observable<any> | undefined;
+  horarios: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.loadUserData();
+  }
+
+  ionViewWillEnter() {
+    this.loadUserData();
+  }
+
+  loadUserData() {
+    this.user$ = this.authService.getUser().pipe(
+      catchError(error => {
+        console.error('Error al obtener los datos del usuario', error);
+        return [];
+      })
+    );
+
+    this.user$.subscribe(data => {
+      this.horarios = data.horario; 
+    });
   }
 
 }
